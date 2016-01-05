a3475b0571b4419c8bd3d90c7ccb3a772a498bbc
Grottel <Sebastian.Grottel@tu-dresden.de>
2016-01-05 10:50:05 +0100

    Prepared storage for the output buffer

---
+++ OutputBuffer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiddenConsole {
+    public class OutputBuffer {
+        public void Add(OutputLineType t, string l) {
+
+        }
+    }
+}
--- SpawnedProcess.cs
+++ SpawnedProcess.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,77 @@ namespace HiddenConsole {
             psi.UseShellExecute = false; // required for stream redirection
             psi.WorkingDirectory = s.WorkingDirectory;
             p = Process.Start(psi);
+            p.EnableRaisingEvents = true;
+            p.StandardOutput.ReadAsync(stdOutBuf, 0, stdOutBuf.Length).ContinueWith(stdOutRead);
+            p.StandardError.ReadAsync(stdErrBuf, 0, stdErrBuf.Length).ContinueWith(stdErrRead);
+            p.Exited += P_Exited;
+            P_Exited(null, null); // fire running changed, just to be sure (most likely a false positive)
         }
         public void Wait() {
             if (p != null) p.WaitForExit();
+        }
+        public OutputBuffer Output { get { return outBuf; } }
+        public bool Running { get { return (p != null) ? !p.HasExited : false; } }
+        public event EventHandler RunningChanged;
+
+        private OutputBuffer outBuf = new OutputBuffer();
+        #region output collection
+        private StringBuilder stdOutStrBuf = new StringBuilder();
+        private StringBuilder stdErrStrBuf = new StringBuilder();
+        private char[] stdOutBuf = new char[200];
+        private char[] stdErrBuf = new char[200];
+        private void stdOutRead(Task<int> size) {
+            this.receiveBuffer(ref stdOutBuf, ref stdOutStrBuf, size.Result, p.StandardOutput, stdOutRead, StdOutLine);
+        }
+        private void stdErrRead(Task<int> size) {
+            this.receiveBuffer(ref stdErrBuf, ref stdErrStrBuf, size.Result, p.StandardError, stdErrRead, StdErrLine);
+        }
+        private void receiveBuffer(ref char[] charBuf, ref StringBuilder strBuf, int size, StreamReader stream, Action<Task<int>> onRead, Action<string> outLine) {
+            if (size > 0) {
+                strBuf.Append(charBuf, 0, size);
+                this.manageBuffer(ref strBuf, outLine);
+            }
+
+            if (size == 0) {
+                if (p.HasExited) {
+                    if (strBuf.Length > 0) {
+                        strBuf.Append("\n");
+                        this.manageBuffer(ref strBuf, outLine);
+                    }
+                    return;
+                }
+            }
+
+            if (stream.BaseStream.CanRead) {
+                stream.ReadAsync(charBuf, 0, charBuf.Length).ContinueWith(onRead);
+            }
+        }
+        private void manageBuffer(ref StringBuilder strBuf, Action<string> wrt) {
+            strBuf.Replace(Environment.NewLine, "\n");
+            string s = strBuf.ToString();
+            int nlp = s.IndexOf('\n');
+            while (nlp >= 0) {
+                string line = s.Substring(0, nlp);
+                strBuf.Remove(0, nlp + 1);
+                if (wrt != null) {
+                    wrt(line);
+                }
+                s = strBuf.ToString();
+                nlp = s.IndexOf('\n');
+            }
+        }
+        private void StdOutLine(string line) {
+            outBuf.Add(OutputLineType.Output, line);
+        }
+        private void StdErrLine(string line) {
+            outBuf.Add(OutputLineType.Error, line);
+        }
+        #endregion
+        private void P_Exited(object sender, EventArgs e) {
+            EventHandler eh = RunningChanged;
+            if (eh != null) {
+                eh(this, null);
+            }
         }
         private Process p = null;
     }
